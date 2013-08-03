742aaf242a8e8e1f24b62dda3fb818d3c2c35cbe
Canh Doan <canh.doanthanh@asnet.com.vn>
2013-08-03 08:38:02 +0700

    - Add ADS shading demo

---
+++ LightingDemos/LightingDemos/src/ADSshading.h
@@ -0,0 +1,66 @@
+///////////////////////////////////////////////////////////////////////////////
+// Copyright (C) 2010 AsNet Co., Ltd.
+// All Rights Reserved. These instructions, statements, computer
+// programs, and/or related material (collectively, the "Source")
+// contain unpublished information proprietary to AsNet Co., Ltd
+// which is protected by US federal copyright law and by 
+// international treaties. This Source may NOT be disclosed to 
+// third parties, or be copied or duplicated, in whole or in 
+// part, without the written consent of AsNet Co., Ltd.
+///////////////////////////////////////////////////////////////////////////////
+// Author: Canh Doan
+///////////////////////////////////////////////////////////////////////////////
+
+#ifndef APP_ADSSHADING_H
+#define APP_ADSSHADING_H
+
+/**
+ * ADS is abbreviato of Ambient-Diffuse-Specular
+ * This is an example about ADS effect, it's use all three components
+ * ambient, diffuse and specular.
+ */
+class ADSShading
+{
+public:
+	/**
+	 * The constructor for this class
+	 */
+	ADSShading(void);
+
+	/**
+	 * The destructor of this class
+	 */
+	~ADSShading(void);
+
+	/**
+	 * The construct method
+	 */
+	void Construct(void);
+
+	/**
+	 * Update method
+	 */
+	void Update(int w, int h);
+
+	/**
+	 * Render method
+	 */
+	void Render(void);
+
+
+private:
+	// Shader program
+	Shader				*m_pShader;
+	// Object to render
+	OBJECT_3DS			m_Object;
+	// List attributes location
+
+	// List vertex buffer
+
+	// Variable for storage data of uniform
+};
+
+
+#endif // APP_ADSSHADING_H
+
+///////////////////////////////////////////////////////////////////////////////
